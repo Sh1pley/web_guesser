e4a3a1819696e58e523427b3e1a4270cfdd62891
Jesse Shipley <jesse.radack@gmail.com>
2016-11-16 09:13:43 -0700

    added colors

--- web_guesser.rb
+++ web_guesser.rb
@@ -2,17 +2,23 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 SECRET_NUMBER = rand(0..100)
+@@background = ""
 
 def check_guess(input)
   if input.to_i > (SECRET_NUMBER + 5)
+    @@background = "crimson"
     "Way Too High!!"
   elsif input.to_i < (SECRET_NUMBER - 5)
+    @@background = "crimson"
     "Way Too Low!!"
   elsif input.to_i > SECRET_NUMBER
+    @@background = "lightpink"
     "Too High!"
   elsif input.to_i < SECRET_NUMBER
+    @@background = "lightpink"
     "Too Low!"
   elsif input.to_i == SECRET_NUMBER
+    @@background = "lime"
     "You got it right! Secret number was #{SECRET_NUMBER}"
   end
 end
@@ -20,6 +26,6 @@ end
 get '/' do
   guess = params["guess"]
   message = check_guess(guess)
-  erb :index, :locals => {:secret_number => SECRET_NUMBER, :message => message}
+  erb :index, :locals => {:secret_number => SECRET_NUMBER, :message => message, :background => @@background}
 end
 
